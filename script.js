c9107518ac36517dc574b3476e68ff7a8c1d674b
Max Tanis <110074@st-maartenscollege.nl>
2021-02-08 09:04:47 +0000

    repl.it aangemaakt en daar bestaande code in gezezt

--- script.js
+++ script.js
@@ -1,60 +1,78 @@
-function setup() {
-	createCanvas(500, 400);
-}
+var screen = 0
 
-var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+function setup() {
+  createCanvas(600, 600);
+}
 
 function draw() {
-	background(225);
-	
-	fill(0);
-	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
-	
-	fill(0, 255, 0);
-	rect(xpos, ypos, 50, 50);
-	
-	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
+  if (screen == 0) {
+    startScreen()
+  }
+  if (screen == 1) {
+    levelScreen()
+  }
+  if (screen == 3) {
+    Level1()
+  }
 }
 
-function keyPressed() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -2;
-			break;
-		case 39:
-		case 68:
-			xspeed = 2;
-			break;
-		case 38:
-		case 87:
-			yspeed = -2;
-			break;
-		case 40:
-		case 83:
-			yspeed = 2;
-			break;
-	}
+function startScreen() {
+  background(96, 157, 255);
+  fill(255)
+  textAlign(CENTER);
+  text('WELCOME TO THE GAME', width / 2, height / 2)
+  text('click to start', width / 2, height / 2 + 20);
 }
 
-function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-		case 38:
-		case 87:
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
-			break;
-	}
+function levelScreen() {
+  // print(mouseX, mouseY)
+  background(255);
+  textSize(32)
+  // eerste rij
+  fill(0, 255, 0);
+  rect(50, 50, 100, 75);
+  fill(255, 0, 255);
+  rect(175, 50, 100, 75);
+  fill(255, 0, 0);
+  rect(300, 50, 100, 75);
+  fill(0, 0, 255);
+  rect(425, 50, 100, 75);
+  // tweede rij
+  fill(0, 255, 0);
+  rect(50, 150, 100, 75);
+  fill(255, 0, 255);
+  rect(175, 150, 100, 75);
+  fill(255, 0, 0);
+  rect(300, 150, 100, 75);
+  fill(0, 0, 255);
+  rect(425, 150, 100, 75);
+  fill(0,0,0)
+  rect(232.5,400,100,75)
+  
+  fill(255,255,255)
+  text('1', 100, 100)
+  text('2', 225, 100)
+  text('3',350, 100)
+  text('4', 475, 100)
+  
+  text('Back', 282.5 ,450)
+}
+
+function Level1() {
+  background(0,0,0)
+}
+
+function mousePressed() {
+  if (screen == 0) {
+    screen = 1
+  } else if (screen == 1) {
+    screen = 2
+  } else if (screen == 2) {
+    if (mouseX > 50,
+    mouseX < 150,
+    mouseY > 50,
+    mouseY < 100)
+    screen = 3
+    cursor(HAND)
+  }
 }
